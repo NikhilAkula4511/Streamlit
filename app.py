c2f7f93bc466cabafc06485944ce33614df96771
NikhilAkula4511 <105285055+NikhilAkula4511@users.noreply.github.com>
2024-03-25 11:30:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+
+st.set_page_config(
+    page_title="Streamlit Application For Perceptronic Model",
+    page_icon="👋",
+)
+st.header("TensorFlow_Nikhil_Elite13")
+st.title("Building An ANN Modelcd ")
+st.sidebar.success("Select a page above.")
+
